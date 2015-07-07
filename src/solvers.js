a53320754b2a035a7a5391eb8e3b0d5febdbe95f
kstrack-grose <kiristrackgrose@gmail.com>
2015-07-06 19:56:50 -0700

    WE SOLVED N-ROOKS

--- src/solvers.js
+++ src/solvers.js
@@ -23,7 +23,7 @@ window.findNRooksSolution = function(n) {
   // set a row variable to 0; 
   // var row = 0;
   // solution array = [];
-  var solutions = [];
+  var solution = [];
   // create new board;
   var newBoard = new Board({'n': n});
   // for all children in solution, call recursive function
@@ -36,11 +36,7 @@ window.findNRooksSolution = function(n) {
     if (!board.hasConflicts()) {
       // if row === n-1 -> push board.rows() to solutions 
       if (row === n-1) {
-        solutions.push(board.rows())
-        // return
-        /* possibly something else here! */
-        var nextBoard = new Board({'n':n});
-        checkIndex(nextBoard,0,1);
+        solutions ++;
         return;
       } else {
         if (column < n-1){
@@ -60,6 +56,9 @@ window.findNRooksSolution = function(n) {
       // if yes conflicts, untoggle row,col
       board.togglePiece(row, column);
       // increment column 
+      if (column === n-1){
+        return;
+      }
       column++;
       // call recursion on board
       checkIndex(board, row, column);
@@ -77,7 +76,55 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0; //fixme
+  // create new board;
+  var newBoard = new Board({'n': n});
+  // for all children in solution, call recursive function
+  // RECURSION 
+  var checkIndex = function(board, row, column) {
+    // toggle the row, at column
+    board.togglePiece(row, column);
+    // check for conflicts
+    // if no conflicts,
+    if (!board.hasConflicts()) {
+      // if row === n-1 -> push board.rows() to solutions 
+      if (row === n-1) {
+        solutionCount ++;
+        // untoggle last piece of solution
+        board.togglePiece(row, column); 
+        return;
+      } else {
+        if (column < n-1){
+          // this will untoggle this first time
+          board.togglePiece(row,column);
+          //make sure column is never >= n
+          checkIndex(board,row,column+1);
+          // this is a retoggle
+          board.togglePiece(row,column);
+        } 
+        // increment row
+        // row++;
+        // column = 0;
+        // column = 0;
+        // call recursion on the whole row
+        checkIndex(board, row+1, 0);
+        // we are untoggling the added piece
+        board.togglePiece(row,column);
+      }
+    } else {
+      // if yes conflicts, untoggle row,col
+      board.togglePiece(row, column);
+      // increment column 
+      if (column === n-1){
+        return;
+      }
+      column++;
+      // call recursion on board
+      checkIndex(board, row, column);
+    }
+  }; 
+    //  var solution = undefined; //fixme
+  checkIndex(newBoard,0,0);
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
